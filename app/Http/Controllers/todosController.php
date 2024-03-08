b19f556d28c444a73f9f13f4d810232703d22be2
madan225767 <rockwithmadan@gmail.com>
2024-03-08 21:51:48 +0545

    this app documentation addedto a folder

--- app/Http/Controllers/todosController.php
+++ app/Http/Controllers/todosController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\todos;
+use App\Models\todos; // using the todos model for database operations.
 
 class todosController extends Controller
 {
